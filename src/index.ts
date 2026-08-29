cfea02cee3ce0fc8bab00cc378caab6dfe1ecae9
Ashik302 <ashikkoirala68@gmail.com>
2026-08-29 14:43:38 +0545

    cors

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://surveybuilder.ashikkoirala.com.np",
+    ],
     credentials: true,
   }),
 );
